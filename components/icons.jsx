2556f4b3ec53dcf69ef80e0ef24b6bcaf60cd197
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-09-03 19:40:51 +0530

    feat: add system components and static assets for homepage and launchpad UI

--- components/icons.jsx
+++ components/icons.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export function AppleIcon({ className = "w-4 h-4" }) {
   return (
     <img
-      src="/images/kavach.png"
+      src="/images/kavach.svg"
       alt="Kavach"
       className={`${className} object-contain select-none pointer-events-none`}
       draggable="false"
